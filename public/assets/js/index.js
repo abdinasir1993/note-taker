b17f20fddc75a7d50286ca603d5c6383db606ca5
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-06-22 14:43:42 +0100

    event target added to button

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,7 +5,7 @@ const shoppingListElement = $("#shopping-list")
 
 const shoppingList = async () => {
 
-
+// not rendering items-list
 
 // fetch items from api
 
@@ -43,32 +43,45 @@ return {
 
 // render list items
 
+const handleClick = (event) => {
+  const target = $(event.target)
+  if (target.is(`button[name="edit-btn"]`)) {
+    const itemId = target.attr("data-items-id")
+    console.log("edit", itemId)
+
+  }
+  if (target.is(`button[name="delete-btn"]`)) {
+    const itemId = target.attr("data-items-id")
+    console.log("delete", itemId)
+    //redirent to /edit/itemId
+
+  }
+}
+
 const renderListItems = (items) => {
-  const listItems = items.map((items) => {
-    return  ` <li
+  const listItems = items.map((item) => {
+    return  `<li
     class="list-group-item d-flex flex-row flex-wrap justify-content-between align-items-center"
-    id="${items.id} "
+    id="${item.id}"
   >
-    <div>${items.name} </div>
+    <div>${item.name} </div>
     <div>
-      <button class="btn btn-secondary" name="edit-btn">edit</button>
-      <button class="btn btn-danger" name="delete-btn">delete</button>
+      <button class="btn btn-secondary" name="edit-btn"data-items-id="${item.id}">edit</button>
+      <button class="btn btn-danger" name="delete-btn"data-items-id="${item.id}">delete</button>
     </div>
-  </li>
-  <li
-    class="list-group-item d-flex flex-row flex-wrap justify-content-between align-items-center"
-    id="b9a949db-c28e-4ce6-819f-bf01f49d8f6b"
-  >`
+  </li>`
   })
   .join("");
-shoppingListElement.append(`<ul class="list-group">${listItems}</ul>`)
+shoppingListElement.append(`<ul class="list-group" id="shopping-list-container">${listItems}</ul>`)
+$("#shopping-list-container").click(handleClick)
 
 };
 
 
 
 const onReady = async () => {
- const items = await getNotesList()
+ const { items } = await shoppingList();
+ renderListItems(items);
 }
-$document.ready(onReady)
+$(document).ready(onReady)
 
