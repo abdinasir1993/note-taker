0f002977b226677bdb586dc7d17b18a09df306e5
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-06-23 13:28:00 +0100

    created a filereadwrite file

--- src/utils/filereadwrite.js
+++ src/utils/filereadwrite.js
@@ -1,11 +1,17 @@
 const fs = require("fs")
 const path = require("path")
+const { fileURLToPath } = require("url")
 
-const readDataFromFile = () => {}
+const readDataFromFile = (fileName) => {
+    filePath = path.join (__dirname, `../data/${fileName}.json`)
+const rawData = fs.readFileSync{filePath, "utf8"}
+return JSON.parse(rawdata)
+}
 
 const writeDataToFile = () => {}
-
+ 
 module.exports = {
     readDataFromFile,
     writeDataToFile
 }
+
