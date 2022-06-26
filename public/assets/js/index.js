1e9b8a427e537dcd89fef48701f7ef643c6a9f8c
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-06-26 18:22:15 +0100

    rendered note form on button click

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,46 +1,68 @@
 console.log("hello from client");
 
-const NotesListElement = $("#notes-list")
+const NotesListElement = $("#notes-list");
 
-const NewNoteElement = $("#icons")
+const NewNoteElement = $("#icons");
 
+const newNoteform = $("#new-note-form");
+
+const listContainer = $("#list-container");
+
+const navbar = $("#navbar");
 
 // not rendering items-list
 
 // fetch items from api
 
 const getNotesList = async () => {
- 
-  const response = await fetch("/api/items")
-  const data = await response.json()
- return data
-  
+  const response = await fetch("/api/items");
+  const data = await response.json();
+  return data;
+};
 
- 
+const handleClickSaveEdit = (event) => {
+  const target = $(event.target);
 
-}
+  if (target.is('i[name= "save-note]')) {
+    const saveNote = target.attr(NewNoteElement);
+    console.log("saveNote");
+  }
+  if (target.is('i[name="new-note"]')) {
+    const newNote = target.attr("save");
+
+    console.log("its working");
+    renderNoteform();
+  }
+};
+
+navbar.click(handleClickSaveEdit);
+
+const handleClick = (event) => {
+  const target = $(event.target);
+  if (target.is(`button[name="edit-btn"]`)) {
+    const itemId = target.attr("data-items-id");
+    console.log("edit", itemId);
+  }
+  if (target.is(`button[name="delete-btn"]`)) {
+    const itemId = target.attr("data-items-id");
+    console.log("delete", itemId);
+  }
+};
+
+// handleSubmit = (event) => {
+
+//   event.preventdefault()
+//   console.log("submit")
+// }
+
+// newNoteform.submit(handSubmit)
 
 // render list items
 
-const handleClick = (event) => {
-  const target = $(event.target)
-  if (target.is(`button[name="edit-btn"]`)) {
-    const itemId = target.attr("data-items-id")
-    console.log("edit", itemId)
-
-  }
-  if (target.is(`button[name="delete-btn"]`)) {
-    const itemId = target.attr("data-items-id")
-    console.log("delete", itemId)
-    
-
-  }
-}
-
 const renderListItems = (items) => {
-  console.log("its working")
-  const listItems = items.map((item) => {
-    return  `<li
+  const listItems = items
+    .map((item) => {
+      return `<li
     class="list-group-item d-flex flex-row flex-wrap justify-content-between align-items-center"
     id="${item.id}"
   >
@@ -49,19 +71,31 @@ const renderListItems = (items) => {
       <button class="btn btn-secondary" name="edit-btn"data-items-id="${item.id}">edit</button>
       <button class="btn btn-danger" name="delete-btn"data-items-id="${item.id}">delete</button>
     </div>
-  </li>`
-  })
-  .join("");
-NotesListElement.append(`<ul class="list-group" id="items-list-container">${listItems}</ul>`)
-$("#items-list-container").click(handleClick)
-
+  </li>`;
+    })
+    .join("");
+  NotesListElement.append(
+    `<ul class="list-group" id="items-list-container">${listItems}</ul>`
+  );
+  $("#items-list-container").click(handleClick);
 };
 
+const renderNoteform = () => {
+  console.log("render");
 
+  listContainer.append(`<div new-notes-form class="col-8">
+  <input
+    class="note-title"
+    placeholder="Note Title"
+    maxlength="28"
+    type="text"
+  />
+  <textarea class="note-textarea" placeholder="Note Text"></textarea>
+</div>`);
+};
 
 const onReady = async () => {
- const { items } = await getNotesList();
- renderListItems(items);
-}
-$(document).ready(onReady)
-
+  const { items } = await getNotesList();
+  renderListItems(items);
+};
+$(document).ready(onReady);
