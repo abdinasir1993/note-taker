4819a2f0c2ad8d2b94f40a9877eb3fd3b00e8c66
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-07-03 17:00:11 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-console.log("hi");
+console.log('hi');
 
 //requiring express library
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const routes = require("./routes/");
+const routes = require('./routes/');
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 //by calling express function this creates an application to set up entire server
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,  "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 // app.use(express.static())
-app.use ('/', routes);
+app.use('/', routes);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
